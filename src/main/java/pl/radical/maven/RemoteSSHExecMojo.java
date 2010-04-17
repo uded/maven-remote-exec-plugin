342c4e7133a33c9dc1c479d990e00d473c0c46df
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-04-17 21:24:36 +0000

    Initial import
    git-svn-id: http://svn.radical.com.pl/maven2/maven-remote-exec-plugin/trunk@2369 01953d5f-b815-0410-ab08-b360c6b68d4c

---
+++ src/main/java/pl/radical/maven/RemoteSSHExecMojo.java
@@ -0,0 +1,228 @@
+package pl.radical.maven;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.Settings;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+/**
+ * A plugin to execute a single command or sequence of commands via SSH.
+ * 
+ * @goal ssh-remote-exec
+ * @requiresProject true
+ * @author <a href="mailto:lukasz.rzanek@radical.com.pl">Łukasz Rżanek</a>
+ */
+public class RemoteSSHExecMojo extends AbstractMojo {
+
+	/**
+	 * The current user system settings for use in Maven.
+	 * 
+	 * @parameter expression="${settings}"
+	 * @readonly
+	 */
+	protected Settings settings;
+
+	/**
+	 * Internal Maven's project
+	 * 
+	 * @parameter expression="${project}"
+	 * @readonly
+	 */
+	protected MavenProject project;
+
+	/**
+	 * A server ID from settings.xml's to be used. This is needed to provide a
+	 * authentication information.
+	 * 
+	 * @parameter expression="${remote-ssh-exec.serverId}";
+	 * @required
+	 */
+	private String serverId;
+
+	/**
+	 * Changes the port used to connect to the remote host.
+	 * 
+	 * @parameter expression="${remote-ssh-exec.port}" default-value=22;
+	 */
+	private int port = 22;
+
+	/**
+	 * Remote host, either DNS name or IP address to connect to.
+	 * 
+	 * @parameter expression="${remote-ssh-exec.host}" required;
+	 * @required
+	 */
+	private String host;
+
+	/**
+	 * Max wait for command to execute in seconds
+	 * 
+	 * @parameter expression="${remote-ssh-exec.host}" default-value=30;
+	 */
+	private int maxWait = 30;
+
+	/**
+	 * A command list to be executed, one by one
+	 * 
+	 * @parameter expression="${remote-ssh-exec.commands}";
+	 */
+	private List<String> commands;
+
+	/**
+	 * Command to be executed on the remote server
+	 * 
+	 * @parameter expression="${remote-ssh-exec.command}";
+	 */
+	private String command;
+
+	private Session session;
+
+	private Thread thread;
+
+	public void execute() throws MojoExecutionException {
+		getLog().debug("Using information from settings.xml about the server: " + serverId);
+
+		final Server server = settings.getServer(serverId);
+		final SSHUserInfo userInfo = new SSHUserInfo();
+		userInfo.setTrust(true);
+
+		try {
+			getLog().debug("Setting up the username to be used in connection: " + server.getUsername());
+			userInfo.setName(server.getUsername());
+			if (server.getPrivateKey() != null) {
+				getLog().debug("Setting up private key settings.xml file");
+				userInfo.setKeyfile(server.getPrivateKey());
+				if (server.getPassphrase() != null) {
+					userInfo.setPassphrase(server.getPassphrase());
+				}
+			} else {
+				getLog().debug("Setting up password from settings.xml file");
+				if (server.getPassword() != null) {
+					userInfo.setPassword(server.getPassword());
+				}
+			}
+
+			final JSch jsch = new JSch();
+			session = jsch.getSession(settings.getServer(serverId).getUsername(), host, port);
+			session.setUserInfo(userInfo);
+			session.setTimeout(maxWait * 1000);
+
+			getLog().info("Connecting to " + host + ":" + port);
+			session.connect();
+
+			if (command != null && !command.isEmpty()) {
+				getLog().debug("Executing single command: " + command);
+				executeCommand(command);
+			} else if (commands != null && commands.size() > 0) {
+				getLog().debug("Using a list of commands to execute. Number of commands: " + commands.size());
+				for (final String command : commands) {
+					executeCommand(command);
+				}
+			} else {
+				throw new MojoExecutionException("At least one command or commandSequence must be specified");
+			}
+
+			getLog().info("Command(s) executed succesfully!");
+		} catch (final JSchException e) {
+			throw new MojoExecutionException("Unable to connec to SSH server", e);
+		} finally {
+			if (session != null && session.isConnected()) {
+				session.disconnect();
+			}
+		}
+	}
+
+	private void executeCommand(String cmd) throws MojoExecutionException {
+		getLog().info("Executing command: " + cmd);
+
+		try {
+			final PipedOutputStream out = new PipedOutputStream();
+			final PipedInputStream readEndOfPipe = new PipedInputStream(out);
+
+			final ChannelExec channel;
+			session.setTimeout(maxWait * 1000);
+			/* execute the command */
+			channel = (ChannelExec) session.openChannel("exec");
+			channel.setCommand(cmd);
+			channel.setOutputStream(out);
+			channel.setExtOutputStream(out);
+			channel.connect();
+
+			// wait for it to finish
+			thread = new Thread() {
+				@Override
+				public void run() {
+					while (!channel.isClosed()) {
+						if (thread == null) {
+							return;
+						}
+						try {
+							byte[] tmp = new byte[1024];
+							while (readEndOfPipe.available() > 0) {
+								int in = readEndOfPipe.read(tmp, 0, 1024);
+								if (in < 0) {
+									break;
+								}
+
+								System.out.print(new String(tmp, 0, in));
+							}
+						} catch (Exception e) {
+							// ignored
+						}
+					}
+				}
+			};
+
+			thread.start();
+			thread.join(maxWait * 1000);
+
+			if (thread.isAlive()) {
+				// ran out of time
+				thread = null;
+				throw new MojoExecutionException("Timeout period exceeded, connection dropped.");
+			} else {
+				// this is the wrong test if the remote OS is OpenVMS,
+				// but there doesn't seem to be a way to detect it.
+				int ec = channel.getExitStatus();
+				if (ec != 0) {
+					String msg = "Remote command failed with exit status " + ec;
+					throw new MojoExecutionException(msg);
+				}
+				channel.disconnect();
+			}
+		} catch (JSchException e) {
+			throw new MojoExecutionException("Error detected while executing the command.", e);
+		} catch (InterruptedException e) {
+			throw new MojoExecutionException("The execution was interrupted.", e);
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to create output writer", e);
+		}
+	}
+}
